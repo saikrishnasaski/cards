433fcb9c7860734b7df93e1c4bbdef5712976ba0
saikrishnasaski <saikrishnachitti19@gmail.com>
2023-08-20 10:22:21 +0530

    added test for newDeck

--- deck_test.go
+++ deck_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+
+	cards := newDeck()
+
+	if len(cards) != 16 {
+
+		t.Errorf("Expected deck length of 16, but got %v", len(cards))
+	}
+}
